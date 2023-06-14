015733f27013fdcbb89c71185b6186d86af73cad
Nazar <nazar378378379379@gmail.com>
2023-06-14 14:44:32 +0300

    develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import { todos } from './data/todos';
 import { TodoTable } from './components/TodoTable';
+import { Link, Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   return <>
     <nav className="navbar is-light px-3">
       <div className="navbar-brand">
-        <a href="/" className="navbar-item">
+        <Link to="/" className="navbar-item">
           <img src="/logo.svg" alt="MA" className="logo" />
-        </a>
+        </Link>
 
-        <a href="/" className="navbar-item">Home</a>
-        <a href="todos" className="navbar-item">Todos</a>
+        <Link to="/" className="navbar-item">Home</Link>
+        <Link to="todos" className="navbar-item">Todos</Link>
       </div>
     </nav>
 
     <div className="section">
-      <h1 className="title">Todos Page</h1>
+      <Routes>
+        <Route
+          path='/'
+          element={<h1 className="title">Home Page</h1>}
+        />
 
-      <TodoTable
-        todos={todos}
-        selectedTodoId={3}
-      />
+        <Route
+          path='todos'
+          element={(
+            <>
+              <h1 className="title">Todos Page</h1>
+
+              <TodoTable
+                todos={todos}
+                selectedTodoId={3}
+              />
+            </>
+          )}
+        />
+      </Routes>
+
     </div>
   </>;
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from 'react-dom/client';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { BrowserRouter } from 'react-router-dom';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './index.css';
 import { App } from './App';
 
 const Root = () => (
-  <App />
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 const root = ReactDOM.createRoot(
